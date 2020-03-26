0faf51f920ac7cdbd6b34ab05aca01fdb0a93cc4
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-03-26 19:38:20 +0100

    Added employee table

--- pel2/create_tables.sql
+++ pel2/create_tables.sql
@@ -9,3 +9,32 @@ CREATE TABLE store(
 );
 ALTER TABLE store OWNER TO ponytojas;
 
+
+DROP TABLE IF EXISTS item CASCADE;
+CREATE TABLE item(
+	barcode            TEXT        NOT NULL,
+	name               TEXT        NOT NULL,
+	type               TEXT        NOT NULL,
+	descripction        TEXT 		NOT NULL,
+	price              INTEGER     NOT NULL,
+	CONSTRAINT item_pk  PRIMARY KEY(barcode)
+);
+ALTER TABLE item OWNER TO ponytojas;
+
+DROP TABLE IF EXISTS employee CASCADE;
+CREATE TABLE employee(
+	id 		        	SERIAL 		NOT NULL,
+	store 				INTEGER 	NOT NULL,
+	dni					TEXT		NOT NULL,
+	first_name			TEXT		NOT NULL,
+	last_name 			TEXT		NOT NULL,
+	position 		   TEXT		   NOT NULL ,
+	salary				INTEGER		NOT NULL,
+	CONSTRAINT employee_pk 	    PRIMARY KEY (id),
+	CONSTRAINT employee_dni		UNIQUE (dni),
+	CONSTRAINT shop_fk 			FOREIGN KEY (store)
+        REFERENCES store(id) MATCH FULL
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+ALTER TABLE employee OWNER TO ponytojas;
