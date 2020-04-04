0879953a64646e73d42819c301906a0ac39ccbcd
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-04-04 18:52:51 +0200

    Innitial commit with the new main python file

---
+++ pel2/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from Company_database.main import create_company_databases as create_company
+
+def main():
+    exit = 0
+    while(not exit):
+        user_input = str
+        print('Welcome to the main program, here you can choose this two options:\n')
+        print('a) Create company database and add all data')
+        print('b) Create the store database with new data')
+        print('s) Exit the program')
+        user_input = input("Choice: ")
+        if(user_input == 'a'):
+            create_company()
+        elif(user_input == 'b'):
+            pass
+        elif(user_input == 's'):
+            exit = 1
+        else:
+            print('Something went wrong')
+            del user_input
+    print('See you later, aligator')
+
+
+if __name__ == "__main__":
+   main()
