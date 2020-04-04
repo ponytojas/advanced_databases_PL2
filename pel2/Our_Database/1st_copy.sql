e67d6b437dfa73c137ad2d991c7d42ac660a58a1
Daniel.Molina <dani99mr31@gmail.com>
2020-04-04 22:09:45 +0200

    fixed errors from copy

--- pel2/Our_Database/1st_copy.sql
+++ pel2/Our_Database/1st_copy.sql
@@ -1,3 +1,7 @@
-COPY employee FROM 'C:/Users/Daniel/Documents/Universidad/GitHub/BBDD_advanzadas/advanced_databases_PL2/pel2/Our_Database/GeneratedCSV/employee_data.csv' WITH (FORMAT csv);
 COPY store FROM 'C:/Users/Daniel/Documents/Universidad/GitHub/BBDD_advanzadas/advanced_databases_PL2/pel2/Our_Database/GeneratedCSV/store_data.csv' WITH (FORMAT csv);
+
+COPY employee(store, dni, first_name, last_name, position, salary) 
+    FROM 'C:/Users/Daniel/Documents/Universidad/GitHub/BBDD_advanzadas/advanced_databases_PL2/pel2/Our_Database/GeneratedCSV/employee_data.csv' 
+    DELIMITER ';' CSV HEADER;
+
 COPY item FROM 'C:/Users/Daniel/Documents/Universidad/GitHub/BBDD_advanzadas/advanced_databases_PL2/pel2/Our_Database/GeneratedCSV/item_data.csv' WITH (FORMAT csv);
