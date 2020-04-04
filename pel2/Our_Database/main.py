2e0504f0dc5084004320950df43e9747152cc1e8
Daniel.Molina <dani99mr31@gmail.com>
2020-04-04 18:54:08 +0200

    Python file for our database

---
+++ pel2/Our_Database/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from Our_Database.CreateEmployees.py import create_employee_csv as create_employees
+
+def create_csv_database():
+    print("Starting to create the data from the store")
+    create_employees()
+    
+
+
+
+if __name__ == "__main__":
+    main()
