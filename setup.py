412d25cfa9fb55b15fd43a6d6706c9af9f9952be
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-04-13 18:11:28 +0200

    Setup the project

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='PEL2 - Advanced Databases',
+    version='1.0.0',
+    description='Full package for PEL2 of Advanced Databases subject at UAH',
+    long_description=readme,
+    author='Daniel Molina && Daniel Villalobos',
+    author_email='daniel.villalobosdel@gmail.com',
+    url='https://github.com/ponytojas/advanced_databases_PL2',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
